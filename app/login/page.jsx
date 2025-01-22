d5c01f3f9900b5407b9c49d2c2e5893e597638e9
NreyesG98 <natalia.reyess1998@gmail.com>
2025-01-22 02:32:58 -0300

    agregar token

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,68 +1,62 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import styles from './Login.module.css';
 
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Login() {
-  const [userType, setUserType] = useState("normal");  // Estado para el tipo de usuario
+function LoginPage() {
+  const [correoApo, setCorreoApo] = useState('');
+  const [contrasenaApo, setContrasenaApo] = useState('');
   const router = useRouter();
 
- 
-  const handleSubmit = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Lógica para manejar el inicio de sesión
-    console.log("Formulario enviado");
-    if (userType === "superusuario") {
-      router.push("/blog");  // Redirigir al dashboard de superusuario
+
+    const response = await fetch('http://localhost:4000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        correo_apo: correoApo,
+        contrasena_apo: contrasenaApo,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('tipo_apo', data.tipo_apo); // Almacena el tipo de usuario
+      alert('Login exitoso');
+      // Redirigir a la página de blog
+      router.push('/blog');
     } else {
-      router.push("/about");  // Redirigir al dashboard de usuario normal
+      alert(data.message);
     }
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.overlay}></div>
-      <div className={styles.loginContainer}>
-        <h1 className={styles.title}>Iniciar Sesión Apoderados</h1>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <label htmlFor="username" className={styles.label}>Usuario:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Ingresa tu usuario"
-            className={styles.input}
-            required
-          />
-          <label htmlFor="password" className={styles.label}>Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Ingresa tu contraseña"
-            className={styles.input}
-            required
-          />
-          <label htmlFor="userType" className={styles.label}>Tipo de Usuario:</label>
-          <select
-            id="userType"
-            name="userType"
-            className={styles.input}
-            value={userType}
-            onChange={(e) => setUserType(e.target.value)}
-            required
-          >
-            <option value="normal">Apoderado</option>
-            <option value="superusuario">Directiva</option>
-          </select>
-          <button type="submit" className={styles.button}>Ingresar</button>
-        </form>
-        <div className={styles.footer}>
-          <p>¿Olvidaste tu contraseña? <a href="/register">Recupérala aquí</a></p>
-          <p>¿No Tienes Cuenta? <a href="/register">¡Regístrate!</a></p>
-        </div>
-      </div>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={correoApo}
+          onChange={(e) => setCorreoApo(e.target.value)}
+          placeholder="Correo"
+          required
+        />
+        <input
+          type="password"
+          value={contrasenaApo}
+          onChange={(e) => setContrasenaApo(e.target.value)}
+          placeholder="Contraseña"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
+
+export default LoginPage;
